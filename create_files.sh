ee5f94e31fad25f5ccbcec24e0ed2282845eff51
Jacob Postema <jacob.postema@deluxe.com>
2023-12-04 10:36:41 -0500

    start day 01

---
+++ create_files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+for y in `seq -w 2015 2022`; do
+  mkdir $DIR/$y
+
+  for i in `seq -w 1 25`; do
+    mkdir $DIR/$y/day-$i
+
+    if [ -z "$(ls -A $DIR/$y/day-$i)" ]; then
+      cp template.rb $DIR/$y/day-$i/day-$i-part-1.rb
+
+      if [ $i -ne '25' ]
+      then
+        cp template.rb $DIR/$y/day-$i/day-$i-part-2.rb
+      fi
+      sed -i "s/\\$/${i}/g" $DIR/$y/day-$i/*.rb
+
+      touch $DIR/$y/day-$i/README.txt
+      touch $DIR/$y/day-$i/day-$i-input.txt
+      chmod +x $y/day-$i/*.rb
+    fi
+  done
+done
---
+++ day-01/day-01-part01.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-$-input.txt", __FILE__)
+input = File.read(file_path)
+
