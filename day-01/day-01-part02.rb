c6008ae855262badbf17685c0fcd5ca90250c2b7
Jacob Postema <jacob.postema@deluxe.com>
2023-12-04 13:19:00 -0500

    part 1 done

--- day-01/day-01-part02.rb
+++ day-01/day-01-part02.rb
@@ -4,8 +4,15 @@
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input = File.readlines(file_path)
 
-cal = input.sum {_1.scan(/[\d]/).values_at(0, -1).join.to_i}
+cal = input.sum { _1.scan(/[\d]/).values_at(0, -1).join.to_i }
+
+def spelled_out(input)
+  case input
+  where input.include?("one")
+  "1"
+end
+end
 
 puts("########## Day 1 2023 ##########")
-puts("Part one solution: #{cal}")
+puts("Part two solution: #{cal}")
 puts("################################")
