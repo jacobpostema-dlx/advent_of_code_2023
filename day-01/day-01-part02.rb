88a9bcb77958cbd54334dafbe8df768eebb616ac
Jacob Postema <jacob.postema@deluxe.com>
2023-12-05 10:45:45 -0500

    part 2 done

--- day-01/day-01-part02.rb
+++ day-01/day-01-part02.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 #!/usr/bin/env ruby
 
-file_path = File.expand_path("../day-01-test02.txt", __FILE__)
+file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input = File.readlines(file_path)
 
-regex =
-cal = input.sum { _1.scan(/[\d]/).values_at(0, -1).join.to_i }
-
-def spelled_out(input)
-end
+DIGITS = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
+cal = input.sum {
+  _1.scan(/(?=(#{DIGITS.keys.join("|")}|\d))/)
+    .flatten.values_at(0, -1)
+    .map { |match| DIGITS[match.to_sym] || match }.join.to_i
+}
 
 puts("########## Day 1 2023 ##########")
 puts("Part two solution: #{cal}")
