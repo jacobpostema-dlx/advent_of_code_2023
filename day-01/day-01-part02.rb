813f9ff5ec4546b7f2bc6ff0ffcef321b50841b9
Jacob Postema <jacob.postema@deluxe.com>
2023-12-05 10:08:21 -0500

    part 2 started

--- day-01/day-01-part02.rb
+++ day-01/day-01-part02.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 #!/usr/bin/env ruby
 
-file_path = File.expand_path("../day-01-input.txt", __FILE__)
+file_path = File.expand_path("../day-01-test02.txt", __FILE__)
 input = File.readlines(file_path)
 
+regex =
 cal = input.sum { _1.scan(/[\d]/).values_at(0, -1).join.to_i }
 
 def spelled_out(input)
-  case input
-  where input.include?("one")
-  "1"
-end
 end
 
 puts("########## Day 1 2023 ##########")
