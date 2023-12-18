f5ed7b812403eba8a15441fdb601c99f7377a0e3
Jacob Postema <jacob.postema@deluxe.com>
2023-12-18 12:01:17 -0500

    day 02 done

--- day-02/day-02-part01.rb
+++ day-02/day-02-part01.rb
@@ -2,10 +2,24 @@
 #!/usr/bin/env ruby
 
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
-input = File.readlines(file_path)
+input = File.read(file_path)
 
-cal = input.sum {_1.scan(/[\d]/).values_at(0, -1).join.to_i}
+RED = 12
+GREEN = 13
+BLUE = 14
 
-puts("########## Day 1 2023 ##########")
-puts("Part one solution: #{cal}")
+sum = input.each_line.sum {
+  red = _1.scan(/(\d+) r/).flatten.map(&:to_i).max
+  green = _1.scan(/(\d+) g/).flatten.map(&:to_i).max
+  blue = _1.scan(/(\d+) b/).flatten.map(&:to_i).max
+
+  if red > RED || green > GREEN || blue > BLUE
+    0
+  else
+    _1.scan(/Game (\d*)/).flatten.first.to_i
+  end
+}
+
+puts("########## Day 2 2023 ##########")
+puts("Part one solution: #{sum}")
 puts("################################")
--- day-02/day-02-part02.rb
+++ day-02/day-02-part02.rb
@@ -2,10 +2,17 @@
 #!/usr/bin/env ruby
 
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
-input = File.readlines(file_path)
+input = File.read(file_path)
 
+sum = input.each_line.sum {
+  red = _1.scan(/(\d+) r/).flatten.map(&:to_i).max
+  green = _1.scan(/(\d+) g/).flatten.map(&:to_i).max
+  blue = _1.scan(/(\d+) b/).flatten.map(&:to_i).max
+
+  red * green * blue
 }
 
+
 puts("########## Day 2 2023 ##########")
-puts("Part two solution: #{cal}")
+puts("Part two solution: #{sum}")
 puts("################################")
