5052e8b9369ab9142843248c46c94cc008253b6e
Jacob Postema <jacob.postema@deluxe.com>
2024-11-22 11:42:47 -0500

    setup for 2024

--- 2024/day-01/day-01-part-1.rb
+++ 2024/day-01/day-01-part-1.rb
@@ -3,3 +3,8 @@
 
 file_path = File.expand_path("../day-$-input.txt", __FILE__)
 input = File.read(file_path)
+
+
+puts("########## Day 01 2024 ##########")
+puts("Part one solution: #{}")
+puts("################################")
--- template.rb
+++ template.rb
@@ -3,3 +3,8 @@
 
 file_path = File.expand_path("../day-$-input.txt", __FILE__)
 input = File.read(file_path)
+
+
+puts("########## Day DD YYYY ##########")
+puts("Part one solution: #{}")
+puts("################################")
