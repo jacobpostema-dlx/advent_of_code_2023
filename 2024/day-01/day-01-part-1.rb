40142fa67f7ed0289ae40f588353f9fd549f6434
Jacob Postema <jacob.postema@deluxe.com>
2024-12-01 14:32:33 -0500

    day01

--- 2024/day-01/day-01-part-1.rb
+++ 2024/day-01/day-01-part-1.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
 #!/usr/bin/env ruby
 
-file_path = File.expand_path("../day-$-input.txt", __FILE__)
-input = File.read(file_path)
+file_path = File.expand_path("../day-01-test_input.txt", __FILE__)
+input = File.readlines(file_path)
+@list1 = []
+@list2 = []
+@sum = 0
 
+def create_lists(input)
+  input.each do |line|
+    row = line.split
+    @list1 << row[0].to_i
+    @list2 << row[1].to_i
+  end
+end
 
+def distance(x, y)
+  (x - y).abs
+end
+
+def main
+  count = @list1.size
+  count.times do
+    @sum = @sum + distance(@list1.min, @list2.min)
+    @list1.delete(@list1.min)
+    @list2.delete(@list2.min)
+  end
+end
+
+create_lists(input)
+main
 puts("########## Day 01 2024 ##########")
-puts("Part one solution: #{}")
+puts("Part one solution: #{@sum}")
 puts("################################")
