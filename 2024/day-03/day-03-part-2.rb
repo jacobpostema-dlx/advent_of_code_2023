43380bb1e8fa2811a0237e504a9960ee25c63242
Jacob Postema <jacob.postema@deluxe.com>
2024-12-03 13:48:39 -0500

    day02 part 1 solution

--- 2024/day-03/day-03-part-2.rb
+++ 2024/day-03/day-03-part-2.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 #!/usr/bin/env ruby
 
-file_path = File.expand_path("../day-$-input.txt", __FILE__)
+file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input = File.read(file_path)
+
+def mul_sum(mul_list)
+  mul_list.map { |mul| mul.scan(/\d{1,3}/).map(&:to_i).reduce(:*) }.sum
+end
+
+mul_list = input.scan('mul\(\d{1,3},\d{1,3}\)')
+input.split("do()")
+
+puts("########## Day 03 2024 ##########")
+puts("Part two solution: #{mul_sum(mul_list)}")
+puts("################################")
