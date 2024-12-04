bd78ebc479c01390cf4125a39173d5eba663b4af
Jacob Postema <jacob.postema@deluxe.com>
2024-12-04 11:19:32 -0500

    day04 part 1 start

--- 2024/day-04/day-04-part-1.rb
+++ 2024/day-04/day-04-part-1.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 #!/usr/bin/env ruby
 
-file_path = File.expand_path("../day-$-input.txt", __FILE__)
-input = File.read(file_path)
+file_path = File.expand_path("../day-04-test_input.txt", __FILE__)
+input = File.readlines(file_path)
+
+horizontal_xmas = input.map { |row| row.scan(/XMAS|SAMX/) }.flatten.compact
+vertical_xmas = input.map(&:chars).transpose.map(&:join).map { |row| row.scan(/XMAS|SAMX/) }.flatten.compact
+
+
+puts("########## Day 03 2024 ##########")
+puts("Part one solution: #{horizontal_xmas.count + vertical_xmas.count}")
+puts("################################")
