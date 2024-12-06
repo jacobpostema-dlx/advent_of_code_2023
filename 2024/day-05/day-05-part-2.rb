ed9776abbfb7a5e69ba89b84e6d33c1c04834a6a
Jacob Postema <jacob.postema@deluxe.com>
2024-12-06 08:34:51 -0500

    day05 part 2 solution

--- 2024/day-05/day-05-part-2.rb
+++ 2024/day-05/day-05-part-2.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 #!/usr/bin/env ruby
 
-file_path = File.expand_path("../day-$-input.txt", __FILE__)
+file_path = File.expand_path("../day-05-input.txt", __FILE__)
 input = File.read(file_path)
+
+rules = Hash.new { |h, k| h[k] = Set.new }
+input.scan(/(\d+)\|(\d+)/) { rules[$1.to_i] << $2.to_i }
+page_updates = input[/(?<=\n\n).*/m].lines.map { _1.scan(/\d+/).map(&:to_i) }
+
+(correct, incorrect) = page_updates.partition do |update|
+  update.map.with_index.all? do |page, i|
+    update[0...i].none? { |before| rules[page].include?(before) }
+  end
+end
+puts correct.inspect
+puts incorrect.inspect
+
+puts("########## Day 05 2024 ##########")
+puts("Part one solution: #{incorrect.map {_1.sort {|a,b| rules[a].include?(b) ? -1 : rules[b].include?(a) ? 1 : 0}}.sum {_1[_1.length/2]}}")
+puts("################################")
+
